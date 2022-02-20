7c184da2e952861bbf303ce3d59de49e0dacd7e7
Gavin Loo <looweiren@gmail.com>
2022-02-20 21:20:13 +0800

    Added State class, moved includes to State.h

--- Game.h
+++ Game.h
@@ -1,15 +1,7 @@
 #ifndef GAME_H
 #define GAME_H
 
-#include <iostream>
-#include <ctime>
-#include <cstdlib>
-
-#include "SFML/Graphics.hpp"
-#include "SFML/Window.hpp"
-#include "SFML/System.hpp"
-#include "SFML/Audio.hpp"
-#include "SFML/Network.hpp"
+#include "State.h"
 
 class Game
 {
---
+++ State.h
@@ -0,0 +1,33 @@
+#ifndef STATE_H
+#define STATE_H
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <vector>
+
+#include "SFML/Graphics.hpp"
+#include "SFML/Window.hpp"
+#include "SFML/System.hpp"
+#include "SFML/Audio.hpp"
+#include "SFML/Network.hpp"
+
+
+class State
+{
+
+private:
+	std::vector<sf::Texture> textures;
+
+
+public:
+	State();
+	virtual ~State();
+
+	/* Pure virtual function.. abstract functions.. */
+	virtual void update() = 0;
+	virtual void render() = 0;
+};
+
+#endif
+
